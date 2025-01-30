0dd6286c9ada8fcd8fe965ed00ec37e3e6bd3bbb
abelfx <163569205+abelfx@users.noreply.github.com>
2025-01-30 15:54:08 +0300

    Created 'Palindrome Number ' by A2SV Hub

---
+++ 30-Jan-2025/Palindrome Number  226712.py
@@ -0,0 +1,18 @@
+# Problem: Palindrome Number  - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        orgx = x
+        reverse = 0
+
+        while (orgx > 0):
+            reverse = reverse*10 + orgx%10
+            orgx = orgx//10
+            
+        if(x == reverse):
+            return True
+        
+        return False
