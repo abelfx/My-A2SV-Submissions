c56740658a2cbbefe5ee68e09e3fa282a2b27ee3
Abel Tesfa <163569205+abelfx@users.noreply.github.com>
2025-02-12 12:09:27 +0300

    Created 'Number of smaller' by A2SV Hub

---
+++ 12-Feb-2025/Number of smaller 259240.py
@@ -0,0 +1,22 @@
+# Problem: Number of smaller - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/B
+
+f, s = map(int, input().split())
+
+first_arr = list(map(int, input().split()))
+second_arr = list(map(int, input().split()))
+
+counter = 0
+result = []
+j = 0
+for i in second_arr:
+    while(j < len(first_arr)):
+        if(i > first_arr[j]):
+            counter += 1
+            j += 1
+        else:
+            break
+    
+    result.append(counter)
+
+print(*result)
+
