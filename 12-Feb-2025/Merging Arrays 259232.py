9564d91d351a4a124d857032e678c0a543e88f03
Abel Tesfa <163569205+abelfx@users.noreply.github.com>
2025-02-12 12:05:05 +0300

    Created 'Merging Arrays' by A2SV Hub

---
+++ 12-Feb-2025/Merging Arrays 259232.py
@@ -0,0 +1,28 @@
+# Problem: Merging Arrays - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/A
+
+f_t, s_t = map(int, input().split())
+
+first_arr = list(map(int, input().split()))
+second_arr = list(map(int, input().split()))
+
+i = 0
+j = 0
+
+result = []
+while (i < len(first_arr) and j < len(second_arr)):
+    if(first_arr[i] < second_arr[j]):
+        result.append(first_arr[i])
+        i += 1
+    else:
+        result.append(second_arr[j])
+        j+= 1
+
+while i < len(first_arr):
+    result.append(first_arr[i])
+    i += 1
+
+while j < len(second_arr):
+    result.append(second_arr[j])
+    j += 1
+
+print(*result)
