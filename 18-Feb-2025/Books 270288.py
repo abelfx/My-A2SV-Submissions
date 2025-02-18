c1a1474d5ad230e8346628d092478b82d8dd4e21
Abel Tesfa <163569205+abelfx@users.noreply.github.com>
2025-02-18 10:14:13 +0300

    Created 'Books' by A2SV Hub

---
+++ 18-Feb-2025/Books 270288.py
@@ -0,0 +1,24 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = map(int, input().split())
+books = list(map(int, input().split()))
+
+left = 0
+prefix_sum = 0
+counter = 0
+
+maxx = float("-inf")
+for right in range(len(books)):
+    prefix_sum += books[right]
+
+    while(prefix_sum > t):
+        prefix_sum -= books[left]
+        left += 1
+        counter -= 1
+    
+    counter += 1
+    right += 1
+
+    maxx = max(maxx, counter)
+
+print(maxx)
