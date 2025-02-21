138843069b86dd92155d4458cd26c48dab978f29
Abel Tesfa <163569205+abelfx@users.noreply.github.com>
2025-02-21 10:37:41 +0300

    Created 'X-Sum' by A2SV Hub

---
+++ 21-Feb-2025/X-Sum 278104.py
@@ -0,0 +1,30 @@
+# Problem: X-Sum - https://codeforces.com/problemset/problem/1676/D
+
+t = int(input())
+test_case = []
+for _ in range(t):
+    n, m = map(int, input().split())
+    grid = [list(map(int, input().split())) for _ in range(n)]
+    test_case.append((n, m, grid))
+
+
+results = []
+for case in test_case:
+    n, m, grid = case
+    
+    main_diagonal = {}
+    anti_diagonal = {}
+
+    for i in range(n):
+        for j in range(m):
+            main_diagonal[i + j] = main_diagonal.get(i + j, 0) + grid[i][j]
+            anti_diagonal[i - j] = anti_diagonal.get(i - j, 0) + grid[i][j]
+    
+    maxx_sum = 0
+
+    for i in range(n):
+        for j in range(m):
+             cur_sum = main_diagonal[i + j] + anti_diagonal[i - j] - grid[i][j]
+             maxx_sum = max(maxx_sum, cur_sum)
+
+    print(maxx_sum)
