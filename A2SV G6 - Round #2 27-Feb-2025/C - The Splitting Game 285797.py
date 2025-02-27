4795d7c78187f43f73116736ea3ecca60bf60911
Abel Tesfa <163569205+abelfx@users.noreply.github.com>
2025-02-27 11:23:49 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 27-Feb-2025/C - The Splitting Game 285797.py
@@ -0,0 +1,26 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import Counter
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input()
+
+    left_counter = Counter()
+    right_counter = Counter(s)
+
+    maxx = float("-inf")
+    for c in s:
+        left_counter[c] += 1
+        right_counter[c] -= 1
+
+        if right_counter[c] == 0:
+            del right_counter[c]
+        
+        maxx = max(maxx, len(left_counter) + len(right_counter))
+    
+    print(maxx)
+
+
+
