4f94507bd88b378531b75ccc92eda564ed8ed8b0
Abel Tesfa <163569205+abelfx@users.noreply.github.com>
2025-03-21 18:23:08 +0300

    Created 'Binary Tree Zigzag Level Order Traversal' by A2SV Hub

---
+++ 21-Mar-2025/Binary Tree Zigzag Level Order Traversal 314721.py
@@ -0,0 +1,37 @@
+# Problem: Binary Tree Zigzag Level Order Traversal - https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        result = []
+
+        q = deque([root])
+        i = 0
+        while q:
+            current_level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                current_level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            
+            if i % 2 != 0:
+                current_level.reverse()
+            result.append(current_level)
+            i += 1
+        
+        return result
+        
+        
+        
+
