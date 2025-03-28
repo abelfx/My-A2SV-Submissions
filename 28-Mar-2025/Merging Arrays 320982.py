9aeff193b81d4d7144969166341c7cf5faa690b9
Abel Tesfa <163569205+abelfx@users.noreply.github.com>
2025-03-28 18:30:00 +0300

    Created 'Merging Arrays' by A2SV Hub

---
+++ 28-Mar-2025/Merging Arrays 320982.py
@@ -0,0 +1,10 @@
+# Problem: Merging Arrays - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/A
+
+n, m = map(int, (input().split()))
+
+lst1 = list(map(int, input().split()))
+lst2 = list(map(int, input().split()))
+
+result = lst1 + lst2
+
+print(*sorted(result))
