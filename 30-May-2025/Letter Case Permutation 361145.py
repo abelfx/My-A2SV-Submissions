3fcdcc64bbf2c6a26869d1360405b195456dc46e
Abel Tesfa <163569205+abelfx@users.noreply.github.com>
2025-05-30 08:31:40 +0300

    Created 'Letter Case Permutation' by A2SV Hub

---
+++ 30-May-2025/Letter Case Permutation 361145.py
@@ -0,0 +1,16 @@
+# Problem: Letter Case Permutation - https://leetcode.com/problems/letter-case-permutation/
+
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        output = [""]
+        for c in s:
+            temp = []
+            if c.isalpha():
+                for o in output:
+                    temp.append(o + c.lower())
+                    temp.append(o + c.upper())
+            else:
+                for o in output:
+                    temp.append(o + c)
+            output = temp
+        return output
