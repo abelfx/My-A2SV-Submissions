73472791ecaa58fbb702ff1c7133a2a0446c3dfa
Abel Tesfa <163569205+abelfx@users.noreply.github.com>
2025-07-31 16:04:21 +0300

    Created 'Evaluate Division' by A2SV Hub

---
+++ 31-Jul-2025/Evaluate Division 380808.py
@@ -0,0 +1,34 @@
+# Problem: Evaluate Division - https://leetcode.com/problems/evaluate-division/
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adj = defaultdict(list)
+
+        for i, eq in enumerate(equations):
+            a, b = eq
+            adj[a].append([b, values[i]])  # map -> list of [b, a/b]
+            adj[b].append([a, 1 / values[i]])
+
+        def bfs(start, end):
+            if start not in adj or end not in adj:
+                return -1
+            
+            q = deque()
+            visit = set()
+
+            q.append([start, 1])
+            visit.add(start)
+
+            while q:
+                n, w = q.popleft()
+                if n == end:
+                    return w
+                
+                for nei, weight in adj[n]:
+                    if nei not in visit:
+                        q.append([nei, w *weight])
+                        visit.add(nei)
+            
+            return -1
+        return [bfs(q[0], q[1]) for q in queries]
+        
